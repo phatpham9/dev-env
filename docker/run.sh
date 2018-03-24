e31376eb4141cbde77910265084825369ed902e7
phatpham9 <phatpham9@gmail.com>
2018-03-25 00:58:49 +0700

    chore: update .gitignore

---
+++ docker/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function loadENV {
+  echo "-> Loading environment variables..."
+  export $( sed "s|\$DIR|$DIR|g" $DIR/.env | grep -v "^#" | xargs )
+  echo "-> Loaded environment variables"
+}
+
+function startServices {
+  echo "-> Starting services..."
+  docker-compose up -d
+  echo "-> Done"
+}
+
+function stopServices {
+  echo "-> Stopping services..."
+  docker-compose down
+  echo "-> Done"
+}
+
+while test $# -gt 0; do
+  case "$1" in
+    --start)
+      loadENV
+      startServices
+      break
+      ;;
+    --stop)
+      loadENV
+      stopServices
+      break
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
